95862f3a1a3a2a00551aca578bba0a61aad8371f
Matthew Linderman <mwl53@mit.edu>
2018-01-13 16:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"  
+  }
+}
